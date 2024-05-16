03816c18f053fa2cc741753fa871ec13942b8eb6
kg-git-hub <148393612+kg-git-hub@users.noreply.github.com>
2024-05-15 19:49:20 -0700

    Update home.js
    removed cookie thing

--- home.js
+++ home.js
@@ -44,5 +44,7 @@ function scheduleInit() {
 }
 
 function show() {
-  document.getElementById("schedule").innerHTML = document.cookie;
+  document.getElementById("schedule").innerHTML = "";
+  let x =document.cookie.split(";")[2];
+  document.getElementById("schedule").innerHTML = x;
 }
